c407516f66b35866302181a52baf30ce58c92e84
Abdullah Al Noman Khan <mdnomanahamed22@gmail.com>
2026-03-24 01:49:35 +0600

    data: team gallery asset links

---
+++ src/data/teamGallery.js
@@ -0,0 +1,12 @@
+export const teamGallery = [
+  "https://res.cloudinary.com/dk2cvqqqj/image/upload/v1774294314/ebddbfc9-282b-4d23-a375-0a4340f74e89_fbf6tl.jpg",
+  "https://res.cloudinary.com/dk2cvqqqj/image/upload/v1774294314/bebec7a6-8747-437f-a96b-6e0ae4a46f22_pz7zxe.jpg",
+  "https://res.cloudinary.com/dk2cvqqqj/image/upload/v1774294314/47476b0b-b93c-4849-b669-d9353bf20442_x1dvdi.jpg",
+  "https://res.cloudinary.com/dk2cvqqqj/image/upload/v1774294313/3f2813da-0a23-4ca3-a244-b69226a988b9_muue0i.jpg",
+  "https://res.cloudinary.com/dk2cvqqqj/image/upload/v1774294313/86cfdeba-9f32-4cd0-8a27-60b58448f839_fvtp26.jpg",
+  "https://res.cloudinary.com/dk2cvqqqj/image/upload/v1774294313/9c117ef4-8287-464c-9eb9-57e4dd990200_qgnc1h.jpg",
+  "https://res.cloudinary.com/dk2cvqqqj/image/upload/v1774294313/9ef1872c-a7fb-4273-b3a9-11b49a33000a_nb8dzb.jpg",
+  "https://res.cloudinary.com/dk2cvqqqj/image/upload/v1774294313/8b6894f5-1146-440c-ac3d-540bc457ffdf_im8aop.jpg",
+  "https://res.cloudinary.com/dk2cvqqqj/image/upload/v1774294313/00a5cd81-e3fb-4ee7-81a6-f86ac5af65c8_obsgsj.jpg",
+  "https://res.cloudinary.com/dk2cvqqqj/image/upload/v1774294313/3dd6b53a-2ffc-4312-8e7e-e6cd7023ceab_mjpvii.jpg"
+];
