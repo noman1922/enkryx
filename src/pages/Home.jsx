37ac913e8728e5647c14fb72497da3487aa2acf1
Abdullah Al Noman Khan <mdnomanahamed22@gmail.com>
2026-03-24 01:49:52 +0600

    update: incorporate new features into home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,29 @@
 import MainLayout from "@/components/MainLayout";
 import Hero from "@/components/Hero";
+import StatsSection from "@/components/StatsSection";
+import LogoSlider from "@/components/LogoSlider";
 import ServicesPreview from "@/components/ServicesPreview";
+import WhyChooseUs from "@/components/WhyChooseUs";
 import WorkProcess from "@/components/WorkProcess";
 import PortfolioTeaser from "@/components/PortfolioTeaser";
 import Testimonials from "@/components/Testimonials";
-import LogoSlider from "@/components/LogoSlider";
 import TeamTeaser from "@/components/TeamTeaser";
+import TeamGallery from "@/components/TeamGallery";
 import CTASection from "@/components/CTASection";
 
 export default function Home() {
   return (
     <MainLayout>
       <Hero />
+      <StatsSection />
       <LogoSlider />
       <ServicesPreview />
+      <WhyChooseUs />
       <WorkProcess />
       <PortfolioTeaser />
       <Testimonials />
       <TeamTeaser />
+      <TeamGallery />
       <CTASection />
     </MainLayout>
   );
